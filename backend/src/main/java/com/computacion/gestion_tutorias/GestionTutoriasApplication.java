f59352a0533f5428ff4926a9c89a366d5542560f
Andres Macas <joseandresmacasvelez@gmail.com>
2023-07-20 21:39:30 -0500

    Cambio

---
+++ backend/src/main/java/com/computacion/gestion_tutorias/GestionTutoriasApplication.java
@@ -0,0 +1,13 @@
+package com.computacion.gestion_tutorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTutoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTutoriasApplication.class, args);
+	}
+
+}
