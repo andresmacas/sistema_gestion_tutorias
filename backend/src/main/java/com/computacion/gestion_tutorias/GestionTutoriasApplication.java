53c2c7fa64b11fed6a841f0da5150cc02ac82cc0
Andres Macas <joseandresmacasvelez@gmail.com>
2023-07-20 21:24:53 -0500

    Solucion de erroes

--- backend/src/main/java/com/computacion/gestion_tutorias/GestionTutoriasApplication.java
+++ backend/src/main/java/com/computacion/gestion_tutorias/GestionTutoriasApplication.java
@@ -1,13 +1,13 @@
-package SgTutorias.sgTutorias;
+package com.computacion.gestion_tutorias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SgTutoriasApplication {
+public class GestionTutoriasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SgTutoriasApplication.class, args);
+		SpringApplication.run(GestionTutoriasApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/computacion/gestion_tutorias/GestionTutoriasApplicationTests.java
+++ backend/src/test/java/com/computacion/gestion_tutorias/GestionTutoriasApplicationTests.java
@@ -1,10 +1,10 @@
-package SgTutorias.sgTutorias;
+package com.computacion.gestion_tutorias;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SgTutoriasApplicationTests {
+class GestionTutoriasApplicationTests {
 
 	@Test
 	void contextLoads() {
