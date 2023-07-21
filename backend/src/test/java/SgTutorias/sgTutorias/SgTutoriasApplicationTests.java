b9e63d319f2098495b2adca22f4dcb917eb3b515
Andres Macas <joseandresmacasvelez@gmail.com>
2023-07-20 20:38:58 -0500

    Cambio de backend por SprinBoot

---
+++ backend/src/test/java/SgTutorias/sgTutorias/SgTutoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package SgTutorias.sgTutorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgTutoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
